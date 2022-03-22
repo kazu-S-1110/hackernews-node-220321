c72bb576a5c41f74c14b9c05491bbb9e6fb53767
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2022-03-23 07:58:52 +0900

    スキーマの拡張方法を学習。

--- src/index.js
+++ src/index.js
@@ -3,13 +3,34 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
+
 const resolvers = {
   Query: {
-    info: () => null,
+    info: () => `This is the API of a Hackernews clone`,
+    feed: () => links,
   },
+  // 実際に下のような動きをするけど実装する必要はない。（こう動くことを理解する）
+  // Link: {
+  //   id: (parent) => parent.id,
+  //   description: (parent) => parent.description,
+  //   // url: (parent) => parent.url,
+  // },
 };
 
 const server = new ApolloServer({
