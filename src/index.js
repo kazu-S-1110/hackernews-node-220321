73ef68f0a0a6a55d3ff7f3e568697a7d12ae39a3
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2022-03-26 21:13:26 +0900

    contextを全てのリゾルバでいじれるようにする。

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
 const fs = require('fs');
 const path = require('path');
 
@@ -29,9 +30,14 @@ const resolvers = {
   },
 };
 
+const prisma = new PrismaClient();
+
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8'),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
