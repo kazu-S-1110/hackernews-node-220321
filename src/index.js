27c09a98019001a56ff1924d73ee124c8223d63e
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2022-03-30 08:02:15 +0900

    contextを編集して認証を通すようにした。結構理解が危なくなってきた。

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
 const fs = require('fs');
 const path = require('path');
+const { getUserId } = require('./utils');
 
 const resolvers = {
   Query: {
@@ -28,8 +29,12 @@ const prisma = new PrismaClient();
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8'),
   resolvers,
-  context: {
-    prisma,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    };
   },
 });
 
